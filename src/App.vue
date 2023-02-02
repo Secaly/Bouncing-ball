95e46b3e819c5378e17d2701c19a34e0a83fa9ff
Secaly <kevin.ledieu.fr@gmail.com>
2023-02-03 00:50:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Ball />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Ball from './components/BallBounce.vue';
+
+@Options({
+  components: {
+    Ball,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  overflow: hidden;
+  margin: 0px;
+}
+</style>
